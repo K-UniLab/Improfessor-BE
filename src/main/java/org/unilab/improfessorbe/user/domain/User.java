a4206aa4f6594c48cedb6ad58a5c1e78a271b9e1
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-06-18 14:47:01 +0900

    [#1] feat: 기본  entity 생성

---
+++ src/main/java/org/unilab/improfessorbe/user/domain/User.java
@@ -0,0 +1,40 @@
+package org.unilab.improfessorbe.user.domain;
+
+import org.unilab.improfessorbe.config.domain.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+
+	private String nickname;
+
+	private String email;
+
+	private String password;
+
+	private String university;
+
+	private String major;
+
+	private Integer freeCount;
+
+	private Integer recommendCount;
+
+	private Role role;
+
+	enum Role {
+		admin, user
+	}
+
+}
