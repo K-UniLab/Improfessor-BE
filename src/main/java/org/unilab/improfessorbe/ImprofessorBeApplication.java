5934436ae2d7332d20e7ad22be57a894430e0eaa
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-06-18 14:01:24 +0900

    Initial setting

---
+++ src/main/java/org/unilab/improfessorbe/ImprofessorBeApplication.java
@@ -0,0 +1,13 @@
+package org.unilab.improfessorbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprofessorBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImprofessorBeApplication.class, args);
+	}
+
+}
