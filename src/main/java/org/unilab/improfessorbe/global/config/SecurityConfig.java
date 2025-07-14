1d391f39edb935dc9a62237391fef9ea34e019c8
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-07-15 00:28:50 +0900

    [#49] feat: 프론트 url 추가

--- src/main/java/org/unilab/improfessorbe/global/config/SecurityConfig.java
+++ src/main/java/org/unilab/improfessorbe/global/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
 		corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(
-			List.of("http://localhost:5173", "https://www.improfessor.co.kr", "https://api.improfessor.co.kr")
+			List.of("http://localhost:5173", "https://www.improfessor.co.kr", "https://api.improfessor.co.kr", "https://improfessor.vercel.app/")
 		);
 
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
