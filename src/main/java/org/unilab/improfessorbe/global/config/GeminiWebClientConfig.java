76c3388b2555e2a4d70d3a271249b9899c3031ae
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-07-26 16:43:18 +0900

    [#54] refactor: webclientconfig 용도에 맞게 gemini이름 추가

--- src/main/java/org/unilab/improfessorbe/global/config/GeminiWebClientConfig.java
+++ src/main/java/org/unilab/improfessorbe/global/config/GeminiWebClientConfig.java
@@ -8,7 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class WebClientConfig {
+public class GeminiWebClientConfig {
 
 	@Value("${gemini.api.base-url}")
 	private String geminiBaseUrl;
