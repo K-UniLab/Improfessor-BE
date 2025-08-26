97956a713d52abc409a256095d24622a6bc505e0
SeokhyunRew <darkknightttt@naver.com>
2025-08-26 21:50:39 +0900

    [#72] refactor : cors 설정 aws > 새로운 서버 도메인으로 변경

--- src/main/java/org/unilab/improfessorbe/global/config/SecurityConfig.java
+++ src/main/java/org/unilab/improfessorbe/global/config/SecurityConfig.java
@@ -92,7 +92,7 @@ public class SecurityConfig {
 		corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(
-			List.of("http://localhost:5173", "https://www.improfessor.co.kr", "https://api.improfessor.co.kr",
+			List.of("http://localhost:5173", "https://www.improfessor.co.kr", "https://api.improfessor.kro.kr",
 				"https://improfessor.vercel.app/")
 		);
 
